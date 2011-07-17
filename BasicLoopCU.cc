873c5ab44b7571037d5010b2b93e90f255702930
wteo <>
2011-07-17 13:24:08 +0000

    pointed to updated reducedTrees location

--- BasicLoopCU.cc
+++ BasicLoopCU.cc
@@ -20,7 +20,7 @@ int main(int argc, char** argv)
 {
  
   //code for sampleName_ and output path
-  TString outputDir = "/cu2/kreis/reducedTrees/V00-02-05/"; //this is where reducedTrees will go
+  TString outputDir = "/cu2/ra2b/reducedTrees/V00-02-05/"; //this is where reducedTrees will go
   stringstream ss;
   TString fileArg;
   *argv++;
