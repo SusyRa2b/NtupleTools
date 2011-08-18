9d15529e463b88031fb75c5f035a98ffd842635a
kreis <>
2011-08-18 21:31:47 +0000

    runs tight and loose in one go, makes text file to be read in drawReducedTrees

--- additionalTools/reweightQCD.C
+++ additionalTools/reweightQCD.C
@@ -4,6 +4,7 @@
 #include "../MiscUtil.cxx"
 #include <cassert>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -11,8 +12,10 @@ using namespace std;
 // input to this is created by drawQCDreweight in drawReducedTrees.C
 //
 
+ofstream filestr; //global cuz i'm lazy
 
-void printABCD(double A, double Aerr, double B, double Berr, double C, double Cerr, double D, double Derr) {
+
+double printABCD(double A, double Aerr, double B, double Berr, double C, double Cerr, double D, double Derr) {
   cout << "A: " << A << " +- " << Aerr << endl;
   cout << "B: " << B << " +- " << Berr << endl;  
   cout << "C: " << C << " +- " << Cerr << endl;
@@ -23,29 +26,42 @@ void printABCD(double A, double Aerr, double B, double Berr, double C, double Ce
   double num = B*D;
   double estimate = num / A;
   double estimateerr= jmt::errAoverB(num,numerr,A,Aerr);
-  
-  cout << "prediction: " << estimate << " +- " << estimateerr << endl;
-  cout << "MC truth  : " << C << " +- " << Cerr << endl;
-  
+ 
   double num1 = C-estimate;
   double perc = num1/estimate;
   double percerr = jmt::errAoverB(C, Cerr, estimate, estimateerr);
+  double combined = sqrt(perc*perc + percerr*percerr)*100;
+
+  cout << "prediction: " << estimate << " +- " << estimateerr << endl;
+  cout << "MC truth  : " << C << " +- " << Cerr << endl;
   cout << "percent   : " << perc*100. << " +- " << percerr*100. << endl;
   cout << "combined  : " << sqrt(perc*perc + percerr*percerr)*100. << endl;
   cout << endl;
+
+  return combined;
 }
 
 
 
-void reweightQCD(){
+void doreweightQCD(TString lt){
 
-  TFile* ifl = TFile::Open("../qcdReweight_loose_50.root","READ"); 
-  //TFile* ifl = TFile::Open("../qcdReweight_loose_30.root","READ"); 
-  //TFile* ifl = TFile::Open("../qcdReweight_tight_50.root","READ"); 
-  // TFile* ifl = TFile::Open("../qcdReweight_tight_30.root","READ"); 
+  TString filename;
+  if(lt == "loose")      filename = "../qcdReweight_loose_30.root";
+  else if(lt == "tight") filename = "../qcdReweight_tight_30.root";
+  else assert(0);
   
+  TFile* ifl = TFile::Open(filename,"READ"); 
+  
+  //For Summer 11 result, d=false and w12same=true.
   bool d = false; //true to double size of correction
-
+  bool w12same = true; //true to use the ge1b weights for ge2b
+  
+  
+  //files for additional studies
+  // TFile* ifl = TFile::Open("../qcdReweight_tight_50.root","READ"); 
+  // TFile* ifl = TFile::Open("../qcdReweight_loose_50.root","READ");   
+  
+  
   TH1D* h_LSB_fDP_antib_njets_qcd    = (TH1D*)ifl->Get("h_LSB_fDP_antib_njets_qcd");
   TH1D* h_LSB_fDP_antib_njets_nonqcd = (TH1D*)ifl->Get("h_LSB_fDP_antib_njets_nonqcd");
   TH1D* h_LSB_fDP_antib_njets_data   = (TH1D*)ifl->Get("h_LSB_fDP_antib_njets_data");
@@ -123,8 +139,6 @@ void reweightQCD(){
   
   
   //make weights
-
-  
   if(d){
     wh_LSB_antib->Add(h_LSB_fDP_antib_njets_data, 2);
     wh_LSB_antib->Add(h_LSB_fDP_antib_njets_nonqcd, -2);
@@ -233,7 +247,7 @@ void reweightQCD(){
   h_SIG_pDP_ge1b_njets_qcd_w->Add(h_SIG_pDP_ge1b_njets_qcd);
   h_SIG_pDP_ge1b_njets_qcd_w->Multiply(wh_SIG_ge1b);
   
-  bool w12same = true;
+
   if(!w12same){
     h_SB_fDP_ge2b_njets_qcd_w->Add(h_SB_fDP_ge2b_njets_qcd);
     h_SB_fDP_ge2b_njets_qcd_w->Multiply(wh_SB_ge2b);
@@ -263,7 +277,9 @@ void reweightQCD(){
   double B, Berr;
   double C, Cerr;
   double D, Derr;
+  double uc, wc;
   
+  //ge1b, SB
   cout << "ge1b, SB: unweighted" << endl;
   A = h_LSB_fDP_antib_njets_qcd->Integral();
   B = h_LSB_pDP_antib_njets_qcd->Integral();
@@ -273,7 +289,7 @@ void reweightQCD(){
   Berr = jmt::errOnIntegral(h_LSB_pDP_antib_njets_qcd);
   Cerr = jmt::errOnIntegral(h_SB_pDP_ge1b_njets_qcd);
   Derr = jmt::errOnIntegral(h_SB_fDP_ge1b_njets_qcd);
-  printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
+  uc = printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
   cout << endl;
   
   cout << "ge1b, SB: weighted" << endl;
@@ -285,9 +301,13 @@ void reweightQCD(){
   Berr = jmt::errOnIntegral(h_LSB_pDP_antib_njets_qcd_w);
   Cerr = jmt::errOnIntegral(h_SB_pDP_ge1b_njets_qcd_w);
   Derr = jmt::errOnIntegral(h_SB_fDP_ge1b_njets_qcd_w);
-  printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
+  wc = printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
   cout << endl;
 
+  filestr << "ge1b" << lt << "sb " << uc << " " << wc << endl;
+
+
+  //ge1b, SIG
   cout << "ge1b, SIG: unweighted" << endl;
   A = h_LSB_fDP_antib_njets_qcd->Integral();
   B = h_LSB_pDP_antib_njets_qcd->Integral();
@@ -297,7 +317,7 @@ void reweightQCD(){
   Berr = jmt::errOnIntegral(h_LSB_pDP_antib_njets_qcd);
   Cerr = jmt::errOnIntegral(h_SIG_pDP_ge1b_njets_qcd);
   Derr = jmt::errOnIntegral(h_SIG_fDP_ge1b_njets_qcd);
-  printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
+  uc = printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
   cout << endl;
 
   cout << "ge1b, SIG: weighted" << endl;
@@ -309,9 +329,13 @@ void reweightQCD(){
   Berr = jmt::errOnIntegral(h_LSB_pDP_antib_njets_qcd_w);
   Cerr = jmt::errOnIntegral(h_SIG_pDP_ge1b_njets_qcd_w);
   Derr = jmt::errOnIntegral(h_SIG_fDP_ge1b_njets_qcd_w);
-  printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);  
+  wc = printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);  
   cout << endl;
 
+  filestr << "ge1b" << lt << "sig " << uc << " " << wc << endl;
+
+
+  //ge2b, SB
   cout << "ge2b, SB: unweighted" << endl;
   A = h_LSB_fDP_antib_njets_qcd->Integral();
   B = h_LSB_pDP_antib_njets_qcd->Integral();
@@ -321,7 +345,7 @@ void reweightQCD(){
   Berr = jmt::errOnIntegral(h_LSB_pDP_antib_njets_qcd);
   Cerr = jmt::errOnIntegral(h_SB_pDP_ge2b_njets_qcd);
   Derr = jmt::errOnIntegral(h_SB_fDP_ge2b_njets_qcd);
-  printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
+  uc = printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
   cout << endl;
 
   cout << "ge2b, SB: weighted" << endl;
@@ -333,9 +357,13 @@ void reweightQCD(){
   Berr = jmt::errOnIntegral(h_LSB_pDP_antib_njets_qcd_w);
   Cerr = jmt::errOnIntegral(h_SB_pDP_ge2b_njets_qcd_w);
   Derr = jmt::errOnIntegral(h_SB_fDP_ge2b_njets_qcd_w);
-  printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
+  wc = printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
   cout << endl;
 
+  filestr << "ge2b" << lt << "sb " << uc << " " << wc << endl;
+
+  
+  //ge2b, SIG
   cout << "ge2b, SIG: unweighted" << endl;
   A = h_LSB_fDP_antib_njets_qcd->Integral();
   B = h_LSB_pDP_antib_njets_qcd->Integral();
@@ -345,7 +373,7 @@ void reweightQCD(){
   Berr = jmt::errOnIntegral(h_LSB_pDP_antib_njets_qcd);
   Cerr = jmt::errOnIntegral(h_SIG_pDP_ge2b_njets_qcd);
   Derr = jmt::errOnIntegral(h_SIG_fDP_ge2b_njets_qcd);
-  printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
+  uc = printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
   cout << endl;
   
   cout << "ge2b, SIG: weighted" << endl;
@@ -357,9 +385,11 @@ void reweightQCD(){
   Berr = jmt::errOnIntegral(h_LSB_pDP_antib_njets_qcd_w);
   Cerr = jmt::errOnIntegral(h_SIG_pDP_ge2b_njets_qcd_w);
   Derr = jmt::errOnIntegral(h_SIG_fDP_ge2b_njets_qcd_w);
-  printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
+  wc = printABCD(A, Aerr, B, Berr, C, Cerr, D, Derr);
   cout << endl;
   
+  filestr << "ge2b" << lt << "sig " << uc << " " << wc << endl;
+
   //
   TCanvas* myC = new TCanvas("myC", "myC", 600, 900);
   myC->Divide(2,3);
@@ -375,6 +405,17 @@ void reweightQCD(){
   wh_SIG_ge2b->Draw();
   
 
-  cout << "reweightQCD() is done." << endl;
+  ifl->Close();
 }
 
+void reweightQCD(){
+  
+  filestr.open("../weightedClosure.dat", fstream::in | fstream::trunc);
+
+  doreweightQCD("loose");
+  doreweightQCD("tight");
+  
+  filestr.close();
+
+
+}
