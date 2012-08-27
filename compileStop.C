9a46d5de77a200b6a22eaf49dfd522c529759d94
joshmt <>
2012-08-27 13:22:38 +0000

    script to autocompile

---
+++ compileStop.C
@@ -0,0 +1,12 @@
+{
+
+  gROOT->LoadMacro("TSelectorMultiDraw.C+");
+  gROOT->LoadMacro("CrossSectionTable.cxx+");
+  gROOT->LoadMacro("ConfigurationDescriptions.cxx+");
+  gROOT->LoadMacro("SearchRegion.cxx+");
+  gROOT->LoadMacro("SystInfo.cxx+");
+  gROOT->LoadMacro("SignalEffData.cxx+");
+
+  gROOT->LoadMacro("stopPlots.C++");
+
+}
