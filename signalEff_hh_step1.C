407f102e2adf31593c9978bb5644c860d62e4c1b
Josh <joshua.thompson@cern.ch>
2013-10-23 19:01:54 +0200

    forgot to adjust this for the latest changes

--- signalEff_hh_step1.C
+++ signalEff_hh_step1.C
@@ -9,7 +9,7 @@ void signalEff_hh_step1(TString file,TString options) {
   TString stub= file(file.Index("reducedTree.")+12,file.Length()-file.Index("reducedTree.")-5-12);
 
   bool joinb = options.Contains("joinbtag");
-  bool usebtagsf = !joinb; //can't use btagsf if we're joining b-tag bins
+  bool usebtagsf = !joinb; //don't use btagsf if we're joining b-tag bins
   bool dopdfs=options.Contains("pdfs");
   bool dopu=options.Contains("pushift");
 
@@ -17,11 +17,7 @@ void signalEff_hh_step1(TString file,TString options) {
   if      ( options.Contains("isrup"))   isrmode = kIsrUp;
   else if ( options.Contains("isrdown")) isrmode = kIsrDown;
 
-  BTagMode btagmode = kBTag0;
-  if (options.Contains("btagup")) btagmode = kBTagHfUp;
-  else if (options.Contains("btagdown"))  btagmode = kBTagHfDown;
-
-  signalEff_hbbhbb looper(path,stub,joinb,usebtagsf,dopdfs,dopu,isrmode,btagmode);
+  signalEff_hbbhbb looper(path,stub,joinb,usebtagsf,dopdfs,dopu,isrmode);
   looper.Loop();
 
 
