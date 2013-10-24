f56bc169a484bb3e6069068709aadee4d1c8f258
Josh <joshua.thompson@cern.ch>
2013-10-24 16:42:23 +0200

    add handling of PU uncertainty

--- signalEff_writetxt_run.C
+++ signalEff_writetxt_run.C
@@ -7,7 +7,7 @@ void signalEff_writetxt_run(TString what, TString sample) {
   TString prefix = "eventcounts.";
   TString prefix2="eventcounts.mergebbins.";
 
-  if (what=="counts") writetxt(what,sample,prefix,true,false);
+  if (what=="counts" || what=="PU") writetxt(what,sample,prefix,true,false);
   else {
     writetxt(what,sample,prefix2,true,false);
   }
